20a98e37ae31a2d4a69838bd9ae2b8172deddbea
John Chacpi <johnchacpi479@gmail.com>
2023-02-12 23:21:43 -0500

    styles fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ import menuIcon from '@icons/icon_menu.svg';
 import logo from '@logos/logo_yard_sale.svg';
 import AppContext from '@context/AppContext';
 import shoppingCart from '@icons/icon_shopping_cart.svg';
-import styles from '@styles/Header.module.scss';
+import styles from '@styles/header.module.scss';
 
 const Header = () => {
   const [toggle, setToggle] = useState(false);
