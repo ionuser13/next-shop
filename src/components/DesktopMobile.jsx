8ccf012e70e24600f336f17ea4cafe7f30369916
John Chacpi <johnchacpi479@gmail.com>
2023-03-18 15:06:05 -0500

    changes con mobile menu

--- src/components/DesktopMobile.jsx
+++ src/components/DesktopMobile.jsx
@@ -3,44 +3,47 @@ import Link from 'next/link';
 import styles from '@styles/desktopMobile.module.scss';
 
 const DesktopMobile = () => {
+  const links = [
+    {
+      label: 'All',
+      route: '/main',
+    },
+    {
+      label: 'Clothes',
+      route: '/clothes',
+    },
+    {
+      label: 'Electronics',
+      route: '/electronics',
+    },
+    {
+      label: 'Furnitures',
+      route: '/furnitures',
+    },
+    {
+      label: 'Toys',
+      route: '/toys',
+    },
+    {
+      label: 'Others',
+      route: '/others',
+    },
+  ];
   return (
     <div className={styles.mobileMenu}>
       <ul className={`${styles['mobileMenu-ul']} ${styles['mobileMenu-ul-1']}`}>
-        <li className={`${styles['mobileMenu-ul-li']}`}>
+      <li className={`${styles['mobileMenu-ul-li']}`}>
           <Link href="/" className={`${styles['mobileMenu-ul-li-a']}`}>
             CATEGORIES
           </Link>
         </li>
-        <li className={`${styles['mobileMenu-ul-li']}`}>
-          <Link href="/" className={`${styles['mobileMenu-ul-li-a']}`}>
-            All
-          </Link>
-        </li>
-        <li className={`${styles['mobileMenu-ul-li']}`}>
-          <Link href="/" className={`${styles['mobileMenu-ul-li-a']}`}>
-            Clothes
-          </Link>
-        </li>
-        <li className={`${styles['mobileMenu-ul-li']}`}>
-          <Link href="/" className={`${styles['mobileMenu-ul-li-a']}`}>
-            Electronics
-          </Link>
-        </li>
-        <li className={`${styles['mobileMenu-ul-li']}`}>
-          <Link href="/" className={`${styles['mobileMenu-ul-li-a']}`}>
-            Furnitures
-          </Link>
-        </li>
-        <li className={`${styles['mobileMenu-ul-li']}`}>
-          <Link href="/" className={`${styles['mobileMenu-ul-li-a']}`}>
-            Toys
-          </Link>
-        </li>
-        <li className={`${styles['mobileMenu-ul-li']}`}>
-          <Link href="/" className={`${styles['mobileMenu-ul-li-a']}`}>
-            Other
-          </Link>
-        </li>
+        {links.map(({ label, route }) => (
+          <li className={`${styles['mobileMenu-ul-li']}`} key={route}>
+            <Link href={route} className={`${styles['mobileMenu-ul-li-a']}`}>
+              {label}
+            </Link>
+          </li>
+        ))}
       </ul>
 
       <ul className={`${styles['mobileMenu-ul']}`}>
